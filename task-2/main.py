63283477c962fd2bc16c93f61f05b236f04e7851
ForeverProglamer <jet.shustriy@gmail.com>
2023-09-19 22:05:42 +0300

    task-2: fix mistake in text

--- task-2/main.py
+++ task-2/main.py
@@ -6,7 +6,7 @@ from core import dollars_to_english_text
 def main() -> None:
     while True:
         try:
-            number = float(input('Enter a number (ex. 56789.99): '))
+            number = float(input('Enter a number (e.g. 56789.99): '))
             result = dollars_to_english_text(number)
             print(result)
         except ValueError as e:
