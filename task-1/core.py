779774f9bcb8b55706c8960830ba6381e01fb7bf
ForeverProglamer <jet.shustriy@gmail.com>
2023-09-19 20:58:16 +0300

    task-1: add validation, cover it with tests

--- task-1/core.py
+++ task-1/core.py
@@ -9,6 +9,8 @@ def shift_picture_to_frame_start(
     pic_y: int
 ) -> None:
     """Shifts a picture's elements to the top left corner of a frame."""
+    _validate_input(frame, pic_width, pic_height, pic_x, pic_y)
+    
     if pic_width == 0 or pic_height == 0:
         return
     if pic_x == 0 and pic_y == 0:
@@ -18,3 +20,23 @@ def shift_picture_to_frame_start(
         k = i + pic_y
         h = j + pic_x
         frame[i][j], frame[k][h] = frame[k][h], frame[i][j]
+
+
+def _validate_input(
+    frame: list[list[int]],
+    pic_width: int,
+    pic_height: int,
+    pic_x: int,
+    pic_y: int
+) -> None:
+    if frame == [] or any(len(row) == [] for row in frame):
+        raise ValueError('Cannot shift the picture within an empty frame')
+
+    if pic_width < 0 or pic_height < 0 or pic_x < 0 or pic_y < 0:
+        raise ValueError('Picture parameters cannot be negative values')
+
+    frame_width = len(frame[0])
+    frame_height = len(frame)
+    
+    if pic_x + pic_width > frame_width or pic_y + pic_height > frame_height:
+        raise ValueError('The picture must be within the frame')
--- task-1/test_core.py
+++ task-1/test_core.py
@@ -28,7 +28,35 @@ class ShiftPictureToFrameStart(TestCase):
     
     def test(self):
         for item in self.data:
-            shift_picture_to_frame_start(**item['input'])
-            self.assertListEqual(
-                item['input']['frame'], item['expected_result']
-            )
+            with self.subTest(item=item):
+                shift_picture_to_frame_start(**item['input'])
+                self.assertListEqual(
+                    item['input']['frame'], item['expected_result']
+                )
+
+    def test_raises_value_error_when_frame_is_empty(self):
+        frame = []
+        pic_width = 1
+        pic_height = 1
+        pic_x = 1
+        pic_y = 1
+
+        with self.assertRaises(ValueError):
+            shift_picture_to_frame_start(frame, pic_width, pic_height, pic_x, pic_y)
+    
+    def test_raises_value_error_when_picture_is_out_of_frame(self):
+        frame = [[1, 2], [3, 4]]
+        bad_inputs = [
+            {'pic_width': 3, 'pic_height': 1, 'pic_x': 1, 'pic_y': 1},
+            {'pic_width': 1, 'pic_height': 2, 'pic_x': 1, 'pic_y': 1},
+            {'pic_width': 1, 'pic_height': 1, 'pic_x': 3, 'pic_y': 1},
+            {'pic_width': 1, 'pic_height': 1, 'pic_x': 1, 'pic_y': 3},
+            {'pic_width': -1, 'pic_height': 1, 'pic_x': 1, 'pic_y': 1},
+            {'pic_width': 1, 'pic_height': -1, 'pic_x': 1, 'pic_y': 1},
+            {'pic_width': 1, 'pic_height': 1, 'pic_x': -1, 'pic_y': 1},
+            {'pic_width': 1, 'pic_height': 1, 'pic_x': 1, 'pic_y': -1},
+        ]
+
+        for inpt in bad_inputs:
+            with (self.subTest(inpt=inpt), self.assertRaises(ValueError)):
+                shift_picture_to_frame_start(frame, **inpt)
